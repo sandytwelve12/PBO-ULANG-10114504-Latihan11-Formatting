b3ae168dab85a9a416032eb4036f0fa573ba31e2
Sandy Akbar <55833676+sandytwelve12@users.noreply.github.com>
2019-09-26 05:44:02 -0700

    Add files via upload

---
+++ PBO-ULANG-10114504-Latihan11-Formatting/src/pbo/ulang/pkg10114504/latihan11/formatting/PBOULANG10114504Latihan11Formatting.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10114504.latihan11.formatting;
+
+/**
+ *
+ * @author 
+ * NAMA     : Sandy Akbar
+ * KELAS    : PBO-Ulang
+ * NIM      : 10114504
+ * DESKRIPSI: Program ini berisi program formating
+ */
+public class PBOULANG10114504Latihan11Formatting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 3546764;
+        int iMinus = -i;
+        System.out.println("i : " + i );
+        System.out.printf("%%d : %d%n", i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", iMinus);
+        System.out.printf("%%,10d : %,10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        double f = 5675482.982;
+        System.out.println("i : " + f );
+        System.out.printf("%%f : %f%n", f);
+        System.out.printf("%%.2f : %.2f%n", f);
+        System.out.printf("%%12.2f : %12.2f%n", f);
+        System.out.printf("%%,12.2f : %,12.2f%n", f);
+    }
+    
+}
